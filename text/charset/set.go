b29373e82e8ac1346f89eacdfefb2d02bf74d430
Pouya Kary <k@karyfoundation.org>
2017-11-11 23:34:58 +0330

    charset added

---
+++ text/charset/set.go
@@ -0,0 +1,51 @@
+
+//
+// Copyright (c) 2017-present Kary Foundation, Inc. All rights reserved.
+//   Author: Pouya Kary <k@karyfoundation.org>
+//
+
+//
+// ─── SETUP ──────────────────────────────────────────────────────────────────────
+//
+
+	package charset
+
+//
+// ─── PARENTHESIS ────────────────────────────────────────────────────────────────
+//
+
+	const LeftParenthesisExtension = '⎜'
+	const RightParenthesisExtension = '⎟'
+
+	const LeftParenthesisUpperHook = '⎛'
+	const RightParenthesisUpperHook = '⎞'
+	const LeftParenthesisLowerHook = '⎠'
+	const RightParenthesisLowerHook = '⎝'
+
+//
+// ─── SQUARE BRACKET ─────────────────────────────────────────────────────────────
+//
+
+	const LeftSquareBracketExtension = '⎢'
+	const RightSquareBracketExtension = '⎥'
+
+	const LeftSquareBracketUpperCorner = '⎡'
+	const RightSquareBracketUpperCorner = '⎤'
+	const LeftSquareBracketLowerCorner = '⎣'
+	const RightSquareBracketLowerCorner = '⎦'
+
+//
+// ─── CURLEY BRACKET ─────────────────────────────────────────────────────────────
+//
+
+	const CurleyBracketExtension = '⎪'
+
+	const LeftCurleyBracketUpperHook = '⎧'
+	const RightCurleyBracketUpperHook = '⎫'
+	const LeftCurleyBracketLowerHook = '⎩'
+	const RightCurleyBracketLowerHook = '⎭'
+
+	const LeftCurleyBracketMiddlePiece = '⎨'
+	const RightCurleyBracketMiddlePiece = '⎬'
+
+// ────────────────────────────────────────────────────────────────────────────────
