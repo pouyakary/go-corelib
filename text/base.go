be3a4f1308eac4444999b334bb552c0cd30e0434
Pouya Kary <k@karyfoundation.org>
2017-11-11 21:53:30 +0330

    base of the Text type

---
+++ text/base.go
@@ -0,0 +1,20 @@
+
+//
+// Copyright (c) 2017-present Kary Foundation, Inc. All rights reserved.
+//   Author: Pouya Kary <k@karyfoundation.org>
+//
+
+//
+// ─── SETUP ──────────────────────────────────────────────────────────────────────
+//
+
+	package text
+
+//
+// ─── TEXT TYPE ──────────────────────────────────────────────────────────────────
+//
+
+	// Text is a `[ ]string` made of lines separated by `\n`
+	type Text [ ]string
+
+// ────────────────────────────────────────────────────────────────────────────────
