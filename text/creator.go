1ecf9f336204754d888d8adb72cdf714bc50a8d4
Pouya Kary <k@karyfoundation.org>
2017-11-11 22:42:52 +0330

    text object creator added

---
+++ text/creator.go
@@ -0,0 +1,57 @@
+
+//
+// Copyright (c) 2017-present Kary Foundation, Inc. All rights reserved.
+//   Author: Pouya Kary <k@karyfoundation.org>
+//
+
+//
+// ─── SETUP ──────────────────────────────────────────────────────────────────────
+//
+
+	package text
+
+//
+// ─── CREATE TEXT OBJECT ─────────────────────────────────────────────────────────
+//
+
+	// Create `O(n)` &mdash; Creates a Kary Framework `Text` from plain text `string`
+	func Create( input string ) Text {
+		result        :=  [ ]string { }
+		iterableText  :=  [ ]rune( input )
+		previousChar  :=  rune( 0 )
+		currentLine   :=  ""
+		index         :=  0
+
+		for _, char := range iterableText {
+			switch char {
+			case '\n':
+				if previousChar != '\r' {
+					result = append( result, currentLine )
+					currentLine = ""
+					index++
+				}
+				previousChar = char
+				break
+
+			case '\r', '\v', '\f', '\u0085', '\u2028', '\u2029':
+				previousChar = char
+				result = append( result, currentLine )
+				currentLine = ""
+				index++
+				break
+
+			default:
+				previousChar = char
+				currentLine += string( char )
+				index++
+			}
+		}
+
+		// adding currentline
+		result = append( result, currentLine )
+
+		// done
+		return result
+	}
+
+// ────────────────────────────────────────────────────────────────────────────────
