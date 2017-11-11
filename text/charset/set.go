faaa6ace8d6c520822f634bab92e7c87820ab2a2
Pouya Kary <k@karyfoundation.org>
2017-11-11 23:37:04 +0330

    formatted the code

--- text/charset/set.go
+++ text/charset/set.go
@@ -14,38 +14,38 @@
 // ─── PARENTHESIS ────────────────────────────────────────────────────────────────
 //
 
-	const LeftParenthesisExtension = '⎜'
-	const RightParenthesisExtension = '⎟'
+	const LeftParenthesisExtension 		= "⎜"
+	const RightParenthesisExtension 	= "⎟"
 
-	const LeftParenthesisUpperHook = '⎛'
-	const RightParenthesisUpperHook = '⎞'
-	const LeftParenthesisLowerHook = '⎠'
-	const RightParenthesisLowerHook = '⎝'
+	const LeftParenthesisUpperHook 		= "⎛"
+	const RightParenthesisUpperHook 	= "⎞"
+	const LeftParenthesisLowerHook 		= "⎠"
+	const RightParenthesisLowerHook 	= "⎝"
 
 //
 // ─── SQUARE BRACKET ─────────────────────────────────────────────────────────────
 //
 
-	const LeftSquareBracketExtension = '⎢'
-	const RightSquareBracketExtension = '⎥'
+	const LeftSquareBracketExtension 	= "⎢"
+	const RightSquareBracketExtension 	= "⎥"
 
-	const LeftSquareBracketUpperCorner = '⎡'
-	const RightSquareBracketUpperCorner = '⎤'
-	const LeftSquareBracketLowerCorner = '⎣'
-	const RightSquareBracketLowerCorner = '⎦'
+	const LeftSquareBracketUpperCorner 	= "⎡"
+	const RightSquareBracketUpperCorner = "⎤"
+	const LeftSquareBracketLowerCorner 	= "⎣"
+	const RightSquareBracketLowerCorner = "⎦"
 
 //
 // ─── CURLEY BRACKET ─────────────────────────────────────────────────────────────
 //
 
-	const CurleyBracketExtension = '⎪'
+	const CurleyBracketExtension 		= "⎪"
 
-	const LeftCurleyBracketUpperHook = '⎧'
-	const RightCurleyBracketUpperHook = '⎫'
-	const LeftCurleyBracketLowerHook = '⎩'
-	const RightCurleyBracketLowerHook = '⎭'
+	const LeftCurleyBracketUpperHook 	= "⎧"
+	const RightCurleyBracketUpperHook 	= "⎫"
+	const LeftCurleyBracketLowerHook 	= "⎩"
+	const RightCurleyBracketLowerHook 	= "⎭"
 
-	const LeftCurleyBracketMiddlePiece = '⎨'
-	const RightCurleyBracketMiddlePiece = '⎬'
+	const LeftCurleyBracketMiddlePiece 	= "⎨"
+	const RightCurleyBracketMiddlePiece = "⎬"
 
 // ────────────────────────────────────────────────────────────────────────────────
