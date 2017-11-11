51e1a94313e48ce8e001129ab43f29286b47e03a
Pouya Kary <k@karyfoundation.org>
2017-11-11 19:49:55 +0330

    padding tools added

--- text/spacings.go
+++ text/spacings.go
@@ -61,4 +61,36 @@
 		return string( result )
 	}
 
+//
+// ─── PAD LEFT ───────────────────────────────────────────────────────────────────
+//
+
+	// PadLeft appends space character to the _left_ side of the string `text`
+	// till it's __total__ length becomes `size`. If the specified `size` be less
+	// than the `text`'s length, it will return the original `text` value
+	func PadLeft( text string, size int ) string {
+		textLength := len( text )
+		spacesNeeded := size - textLength
+		if ( spacesNeeded > 0 ) {
+			return Repeat( " ", spacesNeeded ) + text
+		}
+		return text
+	}
+
+//
+// ─── PAD RIGHT ──────────────────────────────────────────────────────────────────
+//
+
+	// PadRight appends space character to the _right_ side of the string `text`
+	// till it's __total__ length becomes `size`. If the specified `size` be less
+	// than the `text`'s length, it will return the original `text` value
+	func PadRight( text string, size int ) string {
+		textLength := len( text )
+		spacesNeeded := size - textLength
+		if ( spacesNeeded > 0 ) {
+			return text + Repeat( " ", spacesNeeded )
+		}
+		return text
+	}
+
 // ────────────────────────────────────────────────────────────────────────────────
