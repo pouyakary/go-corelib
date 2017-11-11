e225385508c25eb7cfc936e627c0db57de4a62d2
Pouya Kary <k@karyfoundation.org>
2017-11-11 19:49:42 +0330

    repeat function added

---
+++ text/repeat.go
@@ -0,0 +1,32 @@
+
+//
+// Copyright (c) 2017-present Kary Foundation, Inc. All rights reserved.
+//   Author: Pouya Kary <k@karyfoundation.org>
+//
+
+//
+// ─── SETUP ──────────────────────────────────────────────────────────────────────
+//
+
+	package text
+
+//
+// ─── REPEAT TEXT ────────────────────────────────────────────────────────────────
+//
+
+	// Repeat contacts a string `text` to itself by the `times` specified.
+	func Repeat( text string, times int ) string {
+		var result = make( [ ]rune, len( text ) * times )
+		globalIndex := 0
+
+		for index := 0; index < times; index++ {
+			for _, char := range text {
+				result[ globalIndex ] = char
+				globalIndex++
+			}
+		}
+
+		return string( result )
+	}
+
+// ────────────────────────────────────────────────────────────────────────────────
