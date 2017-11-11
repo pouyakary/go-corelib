ee6ba14da653de96080b4745040da7784df38a91
Pouya Kary <k@karyfoundation.org>
2017-11-11 22:43:59 +0330

    now inside of the Text type

---
+++ text/trim.go
@@ -0,0 +1,33 @@
+
+//
+// Copyright (c) 2017-present Kary Foundation, Inc. All rights reserved.
+//   Author: Pouya Kary <k@karyfoundation.org>
+//
+
+//
+// ─── SETUP ──────────────────────────────────────────────────────────────────────
+//
+
+	package text
+
+	import "strings"
+
+//
+// ─── MULTI LINE TRIM ────────────────────────────────────────────────────────────
+//
+
+	// CropToSpacedBox removes text all around the string and returns a boxed string
+	func ( text Text ) CropToSpacedBox( ) Text {
+		longestLine := text.LongestLineLength( )
+		resultingLines := make( [ ]string, len( text ) )
+
+		for index, line := range text {
+			trimmedLine := strings.TrimSpace( line )
+			spacedLine := PadRight( trimmedLine, longestLine )
+			resultingLines[ index ] = spacedLine
+		}
+
+		return resultingLines
+	}
+
+// ────────────────────────────────────────────────────────────────────────────────
