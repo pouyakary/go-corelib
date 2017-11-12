f3d53c9014ed289d54acd20fb3a90b4d5a80587d
Pouya Kary <k@karyfoundation.org>
2017-11-12 17:41:13 +0330

    clear screen added

---
+++ terminal/clear.go
@@ -0,0 +1,22 @@
+
+//
+// Copyright (c) 2017-present Kary Foundation, Inc. All rights reserved.
+//   Author: Pouya Kary <k@karyfoundation.org>
+//
+
+//
+// ─── SETUP ──────────────────────────────────────────────────────────────────────
+//
+
+	package terminal
+
+//
+// ─── CLEAR TERMINAL SCREEN ──────────────────────────────────────────────────────
+//
+
+	// Clear cleans the Terminal screen
+	func Clear( ) {
+		print("\033[H\033[2J")
+	}
+
+// ────────────────────────────────────────────────────────────────────────────────
