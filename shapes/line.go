b0cec7aad96a763e632fde9ac90623cea420b6c1
Pouya Kary <k@karyfoundation.org>
2017-11-12 17:26:48 +0330

    single line added

---
+++ shapes/line.go
@@ -0,0 +1,24 @@
+
+//
+// Copyright (c) 2017-present Kary Foundation, Inc. All rights reserved.
+//   Author: Pouya Kary <k@karyfoundation.org>
+//
+
+//
+// ─── SETUP ──────────────────────────────────────────────────────────────────────
+//
+
+	package shapes
+
+	import "kary/core/line"
+
+//
+// ─── CREATE LINE ────────────────────────────────────────────────────────────────
+//
+
+	// CreateLine generates a line shape with a given `width`
+	func CreateLine( width int ) string {
+		return line.Repeat( "─", width )
+	}
+
+// ────────────────────────────────────────────────────────────────────────────────
