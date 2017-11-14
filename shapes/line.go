314144b62640881a4e7f1fedda6a17bd2090cbda
Pouya Kary <k@karyfoundation.org>
2017-11-15 00:36:45 +0330

    added all sorts af single line

--- shapes/line.go
+++ shapes/line.go
@@ -11,14 +11,47 @@
     package shapes
 
     import "kary/core/line"
+	import "kary/core/lines"
+	import "kary/core/shapes/charset"
 
 //
-// ─── CREATE LINE ────────────────────────────────────────────────────────────────
+// ─── HORIZONTAL LINE ────────────────────────────────────────────────────────────
 //
 
-    // CreateLine generates a line shape with a given `width`
-    func CreateLine( width int ) string {
-        return line.Repeat( "─", width )
+    // CreateHorizontalLightLine generates a light horizontal line shape
+	// with a given `width`
+    func CreateHorizontalLightLine( width int ) string {
+        return line.Repeat( charset.BoxDrawingsLightHorizontal, width )
     }
+
+	// CreateHorizontalHeavyLine generates a heavy horizontal line shape
+	// with a given `width`
+	func CreateHorizontalHeavyLine( width int ) string {
+		return line.Repeat( charset.BoxDrawingsHeavyHorizontal, width )
+	}
+
+//
+// ─── VERTICAL LINE ──────────────────────────────────────────────────────────────
+//
+
+	// CreateVerticalLightLine generates a light vertical line shape with a
+	// given `height`
+	func CreateVerticalLightLine( height int ) lines.Lines {
+		result := make( lines.Lines, height )
+		for index := 0; index < height; index++ {
+			result[ index ] = charset.BoxDrawingsLightVertical
+		}
+		return result
+	}
+
+	// CreateVerticalHeavyLine generates a heavy vertical line shape with a
+	// given `height`
+	func CreateVerticalHeavyLine( height int ) lines.Lines {
+		result := make( lines.Lines, height )
+		for index := 0; index < height; index++ {
+			result[ index ] = charset.BoxDrawingsHeavyVertical
+		}
+		return result
+	}
 
 // ────────────────────────────────────────────────────────────────────────────────
